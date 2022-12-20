ca1872943178cfb331750edc0e3c08dcba343306
Dunja Gojković <74664594+dunjagojkovic@users.noreply.github.com>
2022-12-20 22:15:50 +0100

    eureka hostname

--- psp/src/main/java/tim5/psp/PspApplication.java
+++ psp/src/main/java/tim5/psp/PspApplication.java
@@ -3,9 +3,11 @@ package tim5.psp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableFeignClients
 public class PspApplication {
 
 	public static void main(String[] args) {

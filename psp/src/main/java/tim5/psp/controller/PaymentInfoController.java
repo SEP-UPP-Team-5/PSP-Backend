42ab02451d45e2f43e67db987422ad22b1f9444b
Dunja Gojković <74664594+dunjagojkovic@users.noreply.github.com>
2023-01-25 00:45:29 +0100

    get all eureka clients

--- psp/src/main/java/tim5/psp/controller/PaymentInfoController.java
+++ psp/src/main/java/tim5/psp/controller/PaymentInfoController.java
@@ -1,9 +1,13 @@
 package tim5.psp.controller;
 
+import com.netflix.discovery.shared.Applications;
+import com.netflix.eureka.EurekaServerContextHolder;
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +21,9 @@ import tim5.psp.model.PaymentInfo;
 import tim5.psp.service.PaymentInfoService;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -33,8 +39,6 @@ public class PaymentInfoController {
         return new RestTemplate();
     }
 
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
 
     @PostMapping(path = "/create")
     public ResponseEntity<?> createTransaction(@RequestBody CreateTransactionDTO createTransactionDTO){
@@ -47,12 +51,20 @@ public class PaymentInfoController {
     public ResponseEntity<?> sendTransactionInfo(@PathVariable Long transactionId, @RequestBody PaymentMethodDTO paymentMethodDTO){
 
         PaymentInfo transaction = paymentInfoService.sendTransactionInfo(transactionId, paymentMethodDTO);
-        //ServiceInstance serviceInstance = loadBalancerClient.choose("paypal");
-        //URI url = serviceInstance.getUri();
-       // System.out.println(url);
+
+        PeerAwareInstanceRegistry registry = EurekaServerContextHolder.getInstance().getServerContext().getRegistry();
+        Applications applications = registry.getApplications();
+
+        applications.getRegisteredApplications().forEach((registeredApplication) -> {
+            registeredApplication.getInstances().forEach((instance) -> {
+                System.out.println("name:" + instance.getAppName() + "/" + "instanceId:"+ instance.getInstanceId() + "/" + "hostName:" + instance.getHostName() +"/" + "port" + instance.getPort());
+                if(paymentMethodDTO.getMethodName().equals(instance.getAppName()))
+                    System.out.println("Posalji na ovaj servis");
+            });
+        });
+
         String payPalUrl = "http://localhost:8082/orders/create"; // TODO: from payment method
         String bitcoinUrl = "http://localhost:8085/orders/create"; // TODO: from payment method
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         //TODO: headers.setBearerAuth(token);
@@ -69,9 +81,10 @@ public class PaymentInfoController {
         HttpEntity<String> request = new HttpEntity<>(obj.toString(), headers);
 
         String approvalUrl = restTemplate().postForObject(bitcoinUrl, request, String.class);
-        System.out.println("poslato sa psp na paypal");
+        System.out.println("poslato sa psp na servis za placanje");
         System.out.println(approvalUrl);
         return new ResponseEntity<>(approvalUrl, HttpStatus.CREATED);
+
     }
 
 

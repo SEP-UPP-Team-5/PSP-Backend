6c5680b3042be9997d52f775971431683308feb4
Dunja Gojković <74664594+dunjagojkovic@users.noreply.github.com>
2023-02-02 16:15:22 +0100

    prevent xss attack

---
+++ psp/src/main/java/tim5/psp/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package tim5.psp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers()
+                .xssProtection()
+                .and()
+                .contentSecurityPolicy("script-src 'self'");
+        http.csrf().disable();
+        return http.build();
+    }
+}
